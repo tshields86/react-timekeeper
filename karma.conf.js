315a99c738e4ea543a00ce05755d9f7766baebbc
Travis Shields <travis.shields@gmail.com>
2016-07-25 17:20:58 -0400

    Create clock component

--- karma.conf.js
+++ karma.conf.js
@@ -5,9 +5,9 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: true,
     frameworks: ['mocha'],
-    files: ['app/tests/**/*.text.jsx'],
+    files: ['app/tests/**/*.test.jsx'],
     preprocessors: {
-      'app/tests/**/*.text.jsx': ['webpack', 'sourcemap']
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
     },
     reporters: ['mocha'],
     client: {
