7cc0fc9a18c85bd3ec6f55bbd2c4f212ca45eca5
Travis Shields <travis.shields@gmail.com>
2016-07-25 13:36:51 -0400

    Add automatic tests

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+var webpackConfig = require('./webpack.config.js');
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: ['app/tests/**/*.text.jsx'],
+    preprocessors: {
+      'app/tests/**/*.text.jsx': ['webpack', 'sourcemap']
+    },
+    reporters: ['mocha'],
+    client: {
+      mocha: {
+        timeout: '5000'
+      }
+    },
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
