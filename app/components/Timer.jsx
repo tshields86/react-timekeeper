2f978e4ac5943614a0338f27143621f697228c04
Travis Shields <travis.shields@gmail.com>
2016-07-25 16:15:15 -0400

    Add navigation, timer, and countdown components

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <p>Timer Component</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Timer;
