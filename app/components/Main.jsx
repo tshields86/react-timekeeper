be0bc1708faee510777bbab3627de1ea695e8e56
Travis Shields <travis.shields@gmail.com>
2016-07-25 12:58:14 -0400

    Add nav bar and scss styles

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var Navigation = require('Navigation');
 
 var Main = (props) => {
   return (
     <div>
       <div>
         <div>
+          <Navigation/>
           <p>Main.jsx Rendered</p>
           {props.children}
         </div>
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     root: __dirname,
     alias: {
       Main: 'app/components/Main.jsx',
+      Navigation: 'app/components/Nav.jsx',
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
